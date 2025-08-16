cc66783e1523674d4c64f2c6347b10e616961b3a
ANTIBIOTICS <abx@abx.pe.kr>
2025-08-17 00:21:39 +0900

    인자 순서 수정

--- 03-socket-server/src/SocketError.php
+++ 03-socket-server/src/SocketError.php
@@ -14,7 +14,7 @@ final class SocketError extends RuntimeException {
   }
 
   public function isRetryable(): bool {
-    return in_array([ SOCKET_EAGAIN, SOCKET_EWOULDBLOCK, SOCKET_EINTR], $this->getCode());
+    return in_array($this->getCode(), [ SOCKET_EAGAIN, SOCKET_EWOULDBLOCK, SOCKET_EINTR]);
   }
 
   public function isNotError(): bool {
@@ -22,7 +22,7 @@ final class SocketError extends RuntimeException {
   }
 
   public static function fromCode(int $code, ?Throwable $previous = null): self {
-    return new self($code, socket_strerror($code), $previous);
+    return new self(socket_strerror($code), $code, $previous);
   }
 
   public static function fromRawSocket(Socket $socket, ?Throwable $previous = null): self {
